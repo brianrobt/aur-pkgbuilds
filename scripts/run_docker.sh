57ce721dcfd205df3725bfa7e5e15211749dac0f
Brian Thompson <brianrobt@pm.me>
2025-07-25 15:06:52 -0500

    fix(run_docker.sh): update container and image names to use PKGNAME_DIR for consistency and clarity

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 PKGNAME_DIR="$1"
 PKGNAME=$(grep "^pkgname=" "$PKGNAME_DIR/PKGBUILD" | sed 's/pkgname=//' | tr -d '"' | tr -d "'")
-CONTAINER_NAME="${PKGNAME}-builder"
-IMAGE_NAME="${PKGNAME}-aur"
+CONTAINER_NAME="${PKGNAME_DIR}-builder"
+IMAGE_NAME="${PKGNAME_DIR}-aur"
 AUR_URL="ssh://aur@aur.archlinux.org/${PKGNAME}.git"
 
 # Clone the AUR repository
-if [ ! -d $PKGNAME-aur ]; then
-  git clone $AUR_URL $PKGNAME-aur
+if [ ! -d $PKGNAME_DIR-aur ]; then
+  git clone $AUR_URL $PKGNAME_DIR-aur
 fi
 
 # Checkout the latest version
-cd $PKGNAME-aur
+cd $PKGNAME_DIR-aur
 git checkout master
 git pull origin master
 
 # Copy the PKGBUILD and .SRCINFO files to the AUR repository
-cp -r ../$PKGNAME/* .
+cp -r ../$PKGNAME_DIR/* .
 
 # Build the package
 if [ -f Dockerfile ]; then
